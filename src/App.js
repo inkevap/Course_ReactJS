a361180a1b9b5747a82032fa7b6ccd45e8d81659
Kevin Pocon <inkev.ap@gmail.com>
2022-09-27 21:39:09 -0600

    ciclo de vida de un componente funcional

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import ContactListComponent from './Components/container/contact_list'
+import Clockfuncional from './Components/pure/clockfuncional'
+import Clock from './Components/pure/clock';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <ContactListComponent></ContactListComponent>
+        {/* <ContactListComponent></ContactListComponent> */}
+        <Clockfuncional></Clockfuncional>
+        <Clock></Clock>
       </header>
     </div>
   );

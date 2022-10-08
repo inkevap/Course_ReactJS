b40e0a0ccf0dfe075672962062cb29afae3c4a8e
Kevin Pocon <inkev.ap@gmail.com>
2022-10-08 00:05:55 -0600

    Implementacion de botones de like y dislike

--- src/Components/pure/Joke.jsx
+++ src/Components/pure/Joke.jsx
@@ -1,20 +1,27 @@
 
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { getRandomJoke } from '../../services/AxiosService'
 import { Button } from '@mui/material';
 
 const Joke = () => {
-    
-    const [Joke, setJoke] = useState({value: null});
+
+    const [Like, setLike] = useState(0);
+    const [Count, setCount] = useState({
+        JokeCount: 1,
+        LikeCount: 0,
+        DislikeCount: 0
+    });
+    const [Dislike, setDislike] = useState(0);
+    const [Joke, setJoke] = useState({ value: null });
 
     useEffect(() => {
-        obtainJoke();
+        getJoke();
     }, []);
 
-    const obtainJoke = async () => {
-        await getRandomJoke()
+    const getJoke = () => {
+        getRandomJoke()
             .then((response) => {
-                if(response.status === 200){
+                if (response.status === 200) {
                     // console.table(response.data.results[0])
                     console.log("peticion")
                     console.log(response)
@@ -26,12 +33,47 @@ const Joke = () => {
             })
     }
 
+    const LikeSelected = (val) => {
+        if (val === 'Like') {
+            if (Like === Count.LikeCount) {
+                setLike(Count.LikeCount + 1)
+            } else {
+                setLike(Count.LikeCount)
+            }
+        } else {
+            setLike(Count.LikeCount)
+        }
+        if (val === 'Dislike') {
+            if (Dislike === Count.DislikeCount) {
+                setDislike(Count.DislikeCount + 1)
+            } else {
+                setDislike(Count.DislikeCount)
+            }
+        } else {
+            setDislike(Count.DislikeCount)
+        }
+    }
+
+    const NextJoke = () => {
+        getJoke();
+        let tempCount = {
+            JokeCount: Count.JokeCount + 1,
+            LikeCount: Like,
+            DislikeCount: Dislike
+        }
+        setCount(tempCount)
+    }
+
     return (
         <div className='container bg-dark'>
             <div className='row center text-center text-bg-dark'>
-                <h1>Chuck Norris Joke: </h1>
+                <h1>Chuck Norris Joke #{Count.JokeCount}: </h1>
                 <p>{Joke.value}</p>
-                <Button className='' variant="contained" onClick={obtainJoke}>Next Joke</Button>
+                <div className='column'>
+                    <Button className='' variant="contained" onClick={NextJoke}>Next Joke</Button>
+                    <button className='btn btn-success' onClick={() => LikeSelected('Like')}>Liked {Like}</button>
+                    <button className='btn btn-danger' onClick={() => LikeSelected('Dislike')}>Disliked {Dislike}</button>
+                </div>
             </div>
         </div>
     );
--- src/services/AxiosService.js
+++ src/services/AxiosService.js
@@ -1,8 +1,8 @@
 import APIRequest from '../utils/config/axios.config';
 
 
-export function getRandomJoke() {
-    return APIRequest.get('/', {
+export async function getRandomJoke() {
+    return await APIRequest.get('/', {
         validateStatus: function (status) {
           return status < 500; // Resolve only if the status code is less than 500
         }
